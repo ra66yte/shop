ef521a6526fbfec51a02373e85da60e30fcfb38a
ra66yte <you@example.com>
2021-10-08 22:04:00 +0300

    Make some changes

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::get('/main', [App\Http\Controllers\HomeController::class, 'index'])->name
 // Категории
 Route::group(['prefix' => 'categories'], function(){
     Route::get('/', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories_list');
-    Route::get('/add', [App\Http\Controllers\CategoryController::class, 'create'])->name('add_category')->middleware('auth');;
-    Route::post('/save', [App\Http\Controllers\CategoryController::class, 'store'])->name('add_category_save')->middleware('auth');;
-    Route::get('/{id}', [App\Http\Controllers\CategoryController::class, 'show'])->name('show_category')->middleware('auth');;
-    Route::post('/update', [App\Http\Controllers\CategoryController::class, 'update'])->name('category_update')->middleware('auth');;
-    Route::post('/delete', [App\Http\Controllers\CategoryController::class, 'delete'])->name('category_delete')->middleware('auth');;
+    Route::get('/add', [App\Http\Controllers\CategoryController::class, 'create'])->name('add_category')->middleware('auth');
+    Route::post('/save', [App\Http\Controllers\CategoryController::class, 'store'])->name('add_category_save')->middleware('auth');
+    Route::get('/{id}', [App\Http\Controllers\CategoryController::class, 'show'])->name('show_category')->middleware('auth');
+    Route::post('/update', [App\Http\Controllers\CategoryController::class, 'update'])->name('category_update')->middleware('auth');
+    Route::post('/delete', [App\Http\Controllers\CategoryController::class, 'delete'])->name('category_delete')->middleware('auth');
 });
 
 Route::group(['middleware' => 'role:received', 'prefix' => 'panel'], function(){ // Если роль вообще есть, значит есть доступ к панели управления
