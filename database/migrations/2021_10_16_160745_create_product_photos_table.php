8737374e1e109585ee8acd2a9778718bfa588404
ra66yte <rabbyte@ukr.net>
2021-10-20 09:09:28 +0300

    make changes

--- database/migrations/2021_10_16_160745_create_product_photos_table.php
+++ database/migrations/2021_10_16_160745_create_product_photos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ProductsPhoto extends Migration
+class CreateProductPhotosTable extends Migration
 {
     /**
      * Run the migrations.
